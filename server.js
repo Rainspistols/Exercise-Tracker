b6a12ee07303605b3a3c4b4280112cf3db6e8908
Rainspistols <ffly@mail.ru>
2020-11-27 11:57:48 +0100

    testings

--- server.js
+++ server.js
@@ -1,48 +1,126 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const cors = require('cors')
+const { Schema } = mongoose;
+const PORT = process.env.PORT || '8080';
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useMongoClient: true } )
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/build'));
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-
-app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/build/index.html');
 });
 
-
 // Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+// app.use((req, res, next) => {
+//   return next({ status: 404, message: 'not found' });
+// });
+// MongoDB Schema
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  _id: String,
+  count: { type: Number, default: 0 },
+  log: [{ description: String, duration: Number, date: String }],
+});
+
+const User = mongoose.model('user', UserSchema);
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
     // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    errMessage = err.errors[keys[0]].message;
   } else {
     // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
   }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+  res.status(errCode).send(errMessage);
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const postNewUser = (req, res) => {
+  // newShortUrl.save((err, newUrl) => {
+  //   if (err) return res.send(err);
+  //   return res.json({ original_url: newUrl.original_url, short_url: newUrl.short_url });
+  // });
+
+  User.estimatedDocumentCount().exec((err, count) => {
+    const newUser = new User({
+      username: req.body.username,
+      _id: count,
+    });
+
+    newUser.save((err, savedUser) => {
+      if (err) return res.send(err);
+      return res.json(savedUser);
+    });
+  });
+};
+
+const getAllUsers = (req, res) => {
+  User.find({}, (err, users) => {
+    res.json(users);
+  });
+};
+
+const addExercise = (req, res) => {
+  let { userId, description, duration, date } = req.body;
+  if (!date) {
+    date = new Date();
+  }
+
+  User.findById(userId, (err, updatedUser) => {
+    const changes = {
+      date: date,
+      description: description,
+      duration: duration,
+    };
+    updatedUser.log.push(changes);
+    updatedUser.count = updatedUser.log.length;
+    updatedUser.save((err, updatedUser2) => {
+      if (err) return res.json(err);
+      return res.json({
+        ...changes,
+        _id: userId,
+        username: updatedUser2.username,
+      });
+    });
+  });
+};
+
+const getLogByUser = (req, res) => {
+  const { userId, from, to, limit } = req.body;
+
+  User.findById(userId, (err, userFounded) => {
+    return res.json({
+      userFounded,
+    });
+  });
+};
+
+app.post('/api/exercise/new-user', postNewUser);
+app.get('/api/exercise/users', getAllUsers);
+app.post('/api/exercise/add', addExercise);
+app.get('/api/exercise/log', getLogByUser);
+
+const listener = app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
